e7a507b10ecd94b37a2cd51b1ed39b8ceffede0e
nbrucepro <nezabruce@gmail.com>
2023-07-19 16:48:47 +0200

    deployement_ fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,15 @@
 "use client";
 import { useState } from "react";
-// import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+import dynamic from "next/dynamic";
+const Topbar = dynamic(() => import("./scenes/global/Topbar"),{
+  ssr:false
+})
+const Sidebar =dynamic(() => import("./scenes/global/Sidebar"),
+{ssr:false})
+const Dashboard = dynamic(() =>  import("./scenes/dashboard"),
+{ssr:false})
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
-
-import Image from 'next/image'
-
 export default function Home() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
