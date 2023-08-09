4b14348fdc6ecd3116bf63d0ecc96331cd090f09
nbrucepro <nezabruce@gmail.com>
2023-08-09 02:00:44 +0200

    payroll screen

---
+++ src/pages/payroll.jsx
@@ -0,0 +1,110 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../mockData";
+import Header from '../components/Header'
+import { useTheme } from "@mui/material";
+import LayoutAuthenticated from "../layouts/Authenticated";
+import { ReactElement, useEffect } from 'react';
+
+const Payroll = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  useEffect(()=>{
+    console.log("This is payroll @here");
+  },[])
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "registrarId", headerName: "Registrar ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header
+        title="Payroll"
+        subtitle="List of Payroll for Future Reference"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            // backgroundColor: '#0f172a',
+            // color:"white",
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            // backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          rows={mockDataContacts}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
+    </Box>
+  );
+};
+Payroll.getLayout = function getLayout(page) {
+    return <LayoutAuthenticated>{page}</LayoutAuthenticated>;
+  };
+export default Payroll;
